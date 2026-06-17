f4d8ef05ff151fc72d18b1c05be0616a102c9bfe
javahar79 <javahardevops@gmail.com>
2026-06-17 20:56:07 +0000

    fix: rework Terraform install script to init only

--- terraform/scripts/2026-06-17-install-terraform.sh
+++ terraform/scripts/2026-06-17-install-terraform.sh
@@ -3,9 +3,9 @@
 wget -q https://releases.hashicorp.com/terraform/1.8.5/terraform_1.8.5_linux_amd64.zip
 unzip -qo terraform_1.8.5_linux_amd64.zip -d ~/bin && terraform --version
 mkdir -p ~/tf-first-project && cd ~/tf-first-project || exit
-cat > main.tf << 'EOF'
+cat > main.tf <<'EOF'
 terraform { required_providers { local = { source = "hashicorp/local", version = "2.4.0" } } }
 resource "local_file" "test" { content = "terraform works", filename = "${path.module}/test.txt" }
 EOF
-terraform init && terraform apply -auto-approve
-rm -f test.txt terraform.tfstate main.tf
+terraform init
+rm -f main.tf terraform.tfstate*
